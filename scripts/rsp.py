3eb1e634faa43b61d66ec600ac6b9ec37409682c
Zeyu Yao <novodoodle@gmail.com>
2023-10-15 19:49:40 +0800

    Corrected RSP method

--- scripts/rsp.py
+++ scripts/rsp.py
@@ -1,42 +1,113 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 
-def compute_z_scores(coordinates, theta):
-    """
-    Compute z scores for given coordinates and angle theta.
+def generate_polygon(coordinates, is_expressing, theta_bound=[0, 2 * np.pi]):
+    resolution = 1000
+    theta_start, theta_end = theta_bound
+    angle_step = (theta_end - theta_start) / resolution
 
-    Parameters:
-    - coordinates (np.array): Nx2 array of x, y coordinates.
-    - theta (float): angle in radians.
+    foreground_coordinates = coordinates[is_expressing]
+    background_coordinates = coordinates[~is_expressing]
 
-    Returns:
-    - z_scores (np.array): z scores for given coordinates and theta.
-    """
-    x, y = coordinates[:, 0], coordinates[:, 1]
-    z_scores = x * np.cos(theta) + y * np.sin(theta)
-    return z_scores - np.min(z_scores)
+    total_expressing_cells = sum(is_expressing)
 
+    differences = []
 
-def plot_overlapping_histograms(coordinates, is_expressing_array, theta, bins=30):
-    """
-    Plot overlapping histograms for foreground and background z-scores.
+    for theta in np.linspace(theta_start, theta_end, resolution):
+        # Compute histograms
+        foreground_projection = (
+            np.cos(theta) * foreground_coordinates[:, 0]
+            + np.sin(theta) * foreground_coordinates[:, 1]
+        )
+        background_projection = (
+            np.cos(theta) * background_coordinates[:, 0]
+            + np.sin(theta) * background_coordinates[:, 1]
+        )
 
-    Parameters:
-    - coordinates (np.array): Nx2 array of x, y coordinates.
-    - is_expressing_array (np.array): Boolean array indicating whether each coordinate is in the foreground.
-    - theta (float): angle in radians.
-    - bins (int): number of bins for the histogram.
-    """
-    z_scores = compute_z_scores(coordinates, theta)
+        min_val = min(foreground_projection.min(), background_projection.min())
+        max_val = max(foreground_projection.max(), background_projection.max())
 
-    fg_z_scores = z_scores[is_expressing_array]
-    bg_z_scores = z_scores[~is_expressing_array]
+        norm_denom = max_val - min_val
+        foreground_projection = (foreground_projection - min_val) / norm_denom
+        background_projection = (background_projection - min_val) / norm_denom
 
-    plt.hist(bg_z_scores, bins=bins, alpha=0.5, label="Background", color="blue")
-    plt.hist(fg_z_scores, bins=bins, alpha=0.5, label="Foreground", color="red")
-    plt.legend(loc="upper right")
-    plt.title(f"Overlapping Histograms at θ = {np.degrees(theta):.2f}°")
-    plt.xlabel("Z Score")
-    plt.ylabel("Frequency")
-    plt.show()
+        foreground_hist_values, _ = np.histogram(
+            foreground_projection, bins=resolution, range=(0, 1)
+        )
+        background_hist_values, _ = np.histogram(
+            background_projection, bins=resolution, range=(0, 1)
+        )
+
+        # Compute CDFs
+        foreground_cdf = np.cumsum(foreground_hist_values)
+        # foreground_cdf = foreground_cdf / foreground_cdf[-1]
+        foreground_cdf = foreground_cdf / np.max(foreground_cdf)
+
+        background_cdf = np.cumsum(background_hist_values)
+        # background_cdf = background_cdf / background_cdf[-1]
+        background_cdf = background_cdf / np.max(background_cdf)
+
+        # Compute the difference between CDFs
+        difference = foreground_cdf - background_cdf
+        differences.append(np.sum(difference))
+
+    # Construct the polygon
+    angles = []
+    radii = []
+
+    for theta, diff in zip(
+        np.linspace(theta_start, theta_end, resolution), differences
+    ):
+        h = diff / (np.sin(angle_step / 2) * resolution)
+        angles.extend([theta, theta + angle_step])
+        radii.extend([h, h])
+
+    # Normalize radii
+    radii = [r / total_expressing_cells for r in radii]
+
+    segment_areas = []
+    for i in range(0, len(radii) - 1, 2):
+        theta_diff = angles[i + 1] - angles[i]
+        segment_area = 0.5 * theta_diff * (radii[i] ** 2 + radii[i + 1] ** 2)
+        segment_areas.append(segment_area)
+
+    polygon_area = np.sum(segment_areas)
+
+    # Plotly polar plot for the polygon
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatterpolar(
+            r=radii,
+            theta=np.degrees(angles),  # plotly uses degrees for polar plots
+            fill="toself",
+            name="Polygon",
+            line=dict(color="blue"),
+            opacity=0.5,
+        )
+    )
+
+    fig.update_layout(
+        title="Polygon Representation",
+        polar=dict(
+            radialaxis=dict(visible=True, range=[0, 1]),
+            angularaxis=dict(
+                direction="counterclockwise",
+                showticklabels=True,
+                ticks="outside",
+                rotation=0,
+            ),
+        ),
+        annotations=[
+            go.layout.Annotation(
+                x=0,
+                y=0,
+                text=f"Area: {polygon_area:.2f}",
+                showarrow=False,
+                font=dict(size=16),
+            )
+        ],
+        showlegend=True,
+    )
+
+    return fig
